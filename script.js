e7df29cda98358d00eb27945bc32fb55900fc000
Bro-dep <153253648+Bro-dep@users.noreply.github.com>
2024-12-23 21:07:33 -0800

    i did stuff

---
+++ script.js
@@ -0,0 +1,14 @@
+//How to define variables
+let x, y;
+x = 5;
+y = 10;
+
+var z = x + y;
+
+//How to define functions
+function add(x, y) {
+  return x + y;
+}
+
+//how to make objects
+const exampleArrayTest = {firstName: "Billy", lastName: "Bob", age: 100};
