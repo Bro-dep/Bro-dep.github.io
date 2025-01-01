00d498b46236894de8f4a2420576e05054869b11
Bro-dep <153253648+Bro-dep@users.noreply.github.com>
2025-01-01 14:49:42 -0800

    project array is working somewhat
    It seems to work but need to do some testing

--- js/projectArray.js
+++ js/projectArray.js
@@ -10,11 +10,11 @@ const exampleArrayTest3 = { firstName: "Sik", lastName: "aserv", age: 54 };
 const game1 = {
   Name: "Tammys Tamowatches",
   Link: "https://chaffeycomputerscienceclub.github.io/HTML/newSignup",
-  Img: "#",
+  Img: "../Images/TammysTamoWatches.png",
   Dec: "A Small project intended to be a life organizer mixed with game elements. Made with HTML, CSS, and Javascript. It also included database elements using Firebase.",
 };
 
-var projectArray = [game1];
+var projectArray = [game1, game1, game1];
 //#endregion
 
 var sampleArray = [exampleArrayTest, exampleArrayTest2, exampleArrayTest3];
@@ -22,28 +22,45 @@ var sampleArray = [exampleArrayTest, exampleArrayTest2, exampleArrayTest3];
 function DisplayProjectArray() {
   //prints a amount of stuff based on a array
   var amount = projectArray.length;
-  const container = document.getElementById("ProjectCont");
 
-  container.innerHTML = `
-        <div>
-          <h1>First Name</h1>
-          <h1>Last Name</h1>
-          <h1>Age</h1>
-        </div>
+  const container = document.getElementById("ProjectCont");
+  //container.innerHTML = 
+        `
+        <a
+            href="https://chaffeycomputerscienceclub.github.io/HTML/newSignup"
+            class="cardProject"
+          >
+            <img src="../Images/TammysTamoWatches.png" class="center" />
+            <h2>Tammys Tamowatches</h2>
+            <p>
+              A Small project intended to be a life organizer mixed with game
+              elements. Made with HTML, CSS, and Javascript. It also included
+              database elements using Firebase.
+            </p>
+          </a>
         `;
 
   for (let i = 0; i < amount; i++) {
-    //console.log(i);
-    const iD = document.createElement("div");
-    iD.innerHTML = sampleArray[i].firstName;
-    const iD2 = document.createElement("div");
-    iD2.innerHTML = sampleArray[i].lastName;
-    const iD3 = document.createElement("div");
-    iD3.innerHTML = sampleArray[i].age;
-
-    container.appendChild(iD);
-    container.appendChild(iD2);
-    container.appendChild(iD3);
+    //Link box
+    const linkCont = document.createElement("a");
+    linkCont.href = projectArray[i].Link;
+    linkCont.className = "cardProject";
+    //Img
+    const img = document.createElement("img");
+    img.src = projectArray[i].Img;
+    img.className = "center";
+    //H2
+    const h2 = document.createElement("h2");
+    h2.innerHTML = projectArray[i].Name;
+    //P
+    const p = document.createElement("p");
+    p.innerHTML = projectArray[i].Dec;
+    //Appending
+    linkCont.appendChild(img);
+    linkCont.appendChild(h2);
+    linkCont.appendChild(p);
+    //Appending
+    container.appendChild(linkCont);
   }
 }
 DisplayProjectArray();
