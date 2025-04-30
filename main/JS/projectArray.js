d41bfd9b462694d5a428f13f0acab6313428e469
Luna <153253648+Bro-dep@users.noreply.github.com>
2025-04-30 01:12:35 -0700

    re adding main

---
+++ main/JS/projectArray.js
@@ -0,0 +1,80 @@
+// document.getElementById('ProjectCont').addEventListener(()=>{
+//   getProjects();
+// })
+
+//#region getProjects()
+//Copy's a template to allow a dynamic amount of elements
+async function getProjects(id,amount) {
+  try {
+    const response = await fetch("/main/JS/json/projects.json");
+    var projectArray = await response.json();
+    console.log("projectArray");
+    const container = document.getElementById(id);
+    //Math to center remaining links
+    if(amount == undefined){
+      const amount = projectArray.length;
+      console.log(amount);
+    }
+    else{
+      console.log(amount)
+      projectArray = projectArray.slice(0,amount);
+      console.log(projectArray);
+    }
+    projectArray.forEach((project) => {
+      //Link box
+      const linkCont = document.createElement("a");
+      //linkCont.href = project.Link;
+      linkCont.className = "cardProject";
+      //Used for buttons
+      linkCont.id = project.Name;
+      //Link
+      linkCont.href = project.Link;
+      //Img
+      const img = document.createElement("img");
+      img.src = project.Img;
+      img.className = "center ";
+      //img.className +=  project.Shape;
+      //H2
+      const h2 = document.createElement("h2");
+      h2.innerHTML = project.Name;
+      //P
+      const p = document.createElement("p");
+      p.innerHTML = project.Dec;
+      //Appending
+      linkCont.appendChild(img);
+      linkCont.appendChild(h2);
+      linkCont.appendChild(p);
+      //Appending
+      container.appendChild(linkCont);
+
+      linkCont.onclick = () => expandProject(project);
+    });
+  } catch (error) {
+    console.error("Error Loading items: ", error);
+  }
+}
+
+
+//#region Extra junk
+//Clicking on a project
+function expandProject(project) {
+  console.log(project.Name);
+
+  //Expand element
+  const linkCont = document.getElementById(project.Name);
+  if (linkCont.classList.contains("gridSpan")) {
+    //return to normal
+    linkCont.classList.remove("gridSpan");
+  }
+  else {
+    //expand
+    //linkCont.classList.add("gridSpan")
+    //Adding new content
+  }
+}
+//#endregion
+
+//run on load
+getProjects('ProjectCont');
+getProjects('ProjectContFeature',3);
+
